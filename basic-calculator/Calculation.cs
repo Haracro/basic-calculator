16fbf1b4734435dc5cff6576f3c880996e618bad
HARACRO <nayuu.nayuu17@gmail.com>
2022-02-11 00:07:37 +0800

    Added Calculation Method Changed Add,Subtract,Multiply,Divide Methods to Private

--- basic-calculator/Calculation.cs
+++ basic-calculator/Calculation.cs
@@ -10,14 +10,39 @@ namespace basic_calculator
     {
         public Calculation() { }
 
-        public static double Add(double x, double y) => x + y;
-        public static double Subtract(double x, double y) => x - y;
-        public static double Multiply(double x, double y) => x * y;
-        public static double Divide(double x, double y) => x / y;
+        private static double Add(double x, double y) => x + y;
+        private static double Subtract(double x, double y) => x - y;
+        private static double Multiply(double x, double y) => x * y;
+        private static double Divide(double x, double y) => x / y;
 
         public static double Negate(double number) => -number;
 
         public static double ToDecimal(double number) => number / 100;
+
+        public static double Calculate(double x, double y, Operations op)
+        {
+            double result = 0;
+
+            switch (op.GetOperation)
+            {
+                case Operations.OperationsType.Add:
+                    result = Add(x, y);
+                    break;
+                case Operations.OperationsType.Subtract:
+                    result = Subtract(x, y);
+                    break;
+                case Operations.OperationsType.Multiply:
+                    result = Multiply(x, y);
+                    break;
+                case Operations.OperationsType.Divide:
+                    result = Divide(x, y);
+                    break;
+                default:
+                    result = 0;
+                    break;
+            }
+            return result;
+        }
   
     }
 }
